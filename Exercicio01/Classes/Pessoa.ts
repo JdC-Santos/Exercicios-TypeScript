d654cd9886431aba5a1977e381ac298bfb8b2303
Jhonatan da Costa Santos <jdc_santos@outlook.com>
2020-02-08 01:06:49 +0000

    criando clsse pessoa e transpilando para js.

---
+++ Exercicio01/Classes/Pessoa.ts
@@ -0,0 +1,33 @@
+namespace empresa{
+    
+    class Pessoa {
+        private _nome: string|undefined;
+        private _cpf: string;
+        private _dtNascimento: string|undefined;
+
+        constructor(cpf:string){
+            this._cpf = cpf;
+        }
+
+        getNome():string|undefined{
+            return this._nome;
+        }
+
+        setNome(nome: string): void {
+            this._nome = nome;
+        }
+
+        getCpf(): string|undefined{
+            return this._cpf;
+        }
+
+        getDtNascimento():string|undefined{
+            return this._dtNascimento;
+        }
+
+        setDtNascimento(dt:string):void{
+            this._dtNascimento = dt;
+        }
+    }
+
+}
